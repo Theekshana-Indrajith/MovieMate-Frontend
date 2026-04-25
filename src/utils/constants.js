e18db9acd941ab1749c29af838299a9e4e19f2d9
Theekshana-Indrajith <agtheekshanaindrajith@gmail.com>
2026-04-25 22:14:35 +0530

    update backend url to production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-const BASE_URL = 'http://192.168.43.245:5000';
+// const BASE_URL = 'http://192.168.43.245:5000';
+const BASE_URL = 'https://moviemate-backend-3plg.onrender.com';
 export default BASE_URL;
